5bf6dfdf34163ad8f37c2b94921af32da2c8fcdb
Keith Murimi <keith.murimi@student.moringaschool.com>
2024-10-12 20:11:18 +0300

    Removed inquiries, updated the format using prettier, updated css styles for the html, commented out setTimeout for welcomeMessage

--- script.js
+++ script.js
@@ -1,10 +1,13 @@
 document.addEventListener("DOMContentLoaded", function() {
     const welcomeMessage = document.getElementById("welcome-message");
+    const mainContent = document.getElementById('main-content');
     
     setTimeout(() => {
         welcomeMessage.style.opacity = 0;
-        setTimeout(() => {
+        // setTimeout(() => {
             welcomeMessage.style.display = "none";
-        }, 1000);
-    }, 3000);
+        // }, 1000);
+            mainContent.classList.remove('hidden');
+            mainContent.classList.add('show-content');
+    }, 5000);
 });
